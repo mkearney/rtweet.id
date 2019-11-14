e42cedb9d804e7356daaa7a6e7ee489a11e1ef50
mkearney <kearneymw@missouri.edu>
2019-11-14 13:56:42 -0600

    Document id converters

--- R/id2sn.R
+++ R/id2sn.R
@@ -1,3 +1,10 @@
+#' Screen name to user ID
+#'
+#' Convert screen names to user IDs
+#'
+#' @param Input vector of screen names
+#' @return Vector of user IDs
+#' @export
 sn2id <- function(x) {
   o <- tryCatch(
     suppressWarnings(scan(paste0("https://twitter.com/", x), character(),
@@ -11,6 +18,13 @@ sn2id <- function(x) {
     screen_name = x)
 }
 
+#' User ID to screen name
+#'
+#' Convert user IDs to screen names
+#'
+#' @param Input vector of user IDs
+#' @return Vector of screen names
+#' @export
 id2sn <- function(x) {
   o <- tryCatch(
     suppressWarnings(scan(paste0("https://twitter.com/intent/user?user_id=", x),
@@ -24,6 +38,14 @@ id2sn <- function(x) {
     name = paste(sub("<title>", "", o[1], fixed = TRUE), o[2]),
     screen_name = sub(")", "", sub("(@", "", o[3], fixed = TRUE), fixed = TRUE))
 }
+
+#' Status ID to screen name
+#'
+#' Converts Twitter status ID to screen name
+#'
+#' @param twid Input status ID
+#' @return Screen name
+#' @export
 sid2sn <- function(twid) {
   o <- tryCatch(
     httr::HEAD(paste0("https://twitter.com/NA/status/", twid))[["url"]],
