022cb0bf4135bbd01d726ad748790c0fad02608d
mkearney <kearneymw@missouri.edu>
2019-11-14 13:51:43 -0600

    rm rtweet.download dep

--- R/lookup.R
+++ R/lookup.R
@@ -6,17 +6,7 @@
 #' @return A vector of screen names (NAs returned for non-matches)
 #' @export
 lookup_screen_name <- function(x) {
-  sn <- users_handles$screen_name[match(x, users_handles$user_id)]
-  if (!any(is.na(sn))) {
-    return(sn)
-  }
-  u <- rtweet.download:::lookup_users2(x[is.na(sn)])
-  if (NROW(u) == 0) {
-    return(sn)
-  }
-  rtweet.download:::complete("Lookup ", rtweet.download:::cint(sum(is.na(sn))), " users")
-  sn[is.na(sn)] <- u$screen_name[match(x[is.na(sn)], u$user_id)]
-  sn
+  users_handles$screen_name[match(x, users_handles$user_id)]
 }
 
 #' Lookup user IDs
@@ -29,18 +19,7 @@ lookup_screen_name <- function(x) {
 lookup_user_id <- function(x) {
   ## create lookup vectors that include all-lowercase version
   muisn <- luvalv(users_handles$user_id, users_handles$screen_name)
-  sn <- muisn$sn[match(x, muisn$ui)]
-  if (!any(is.na(sn))) {
-    return(sn)
-  }
-  u <- rtweet.download:::lookup_users2(x[is.na(sn)])
-  if (NROW(u) == 0) {
-    return(sn)
-  }
-  rtweet.download:::complete("Lookup ", rtweet.download:::cint(sum(is.na(sn))), " users")
-  muisn <- luvalv(u$user_id, u$screen_name)
-  sn[is.na(sn)] <- muisn$sn[match(x[is.na(sn)], muisn$ui)]
-  sn
+  muisn$sn[match(x, muisn$ui)]
 }
 
 #' Lookup bioguide ID
