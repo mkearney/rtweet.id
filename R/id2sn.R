60ca226512bf7678e35186f1ccbb96a17d5098be
mkearney <kearneymw@missouri.edu>
2019-11-18 12:41:45 -0600

    Rename

--- R/id2sn.R
+++ R/id2sn.R
@@ -6,14 +6,18 @@
 #' @return Vector of user IDs
 #' @export
 sn2id <- function(x) {
+  do.call("rbind", dapr::lap(x, sn2id_))
+}
+
+sn2id_ <- function(x) {
   o <- tryCatch(
     suppressWarnings(scan(paste0("https://twitter.com/", x), character(),
       skip = 1005, nmax = 4, quiet = TRUE)),
-    error = function(e) list(user_id = NA_character_, screen_name = x))
-  if (is.list(o)) {
+    error = function(e) tibble::tibble(user_id = NA_character_, screen_name = x))
+  if (is.data.frame(o)) {
     return(o)
   }
-  list(user_id = sub("\">", "", sub("data-user-id=\"", "", o[4], fixed = TRUE),
+  tibble::tibble(user_id = sub("\">", "", sub("data-user-id=\"", "", o[4], fixed = TRUE),
     fixed = TRUE),
     screen_name = x)
 }
@@ -26,33 +30,43 @@ sn2id <- function(x) {
 #' @return Vector of screen names
 #' @export
 id2sn <- function(x) {
+  do.call("rbind", dapr::lap(x, id2sn_))
+}
+
+id2sn_ <- function(x) {
   o <- tryCatch(
     suppressWarnings(scan(paste0("https://twitter.com/intent/user?user_id=", x),
-      character(), skip = 8, nmax = 5, quiet = TRUE)),
-    error = function(e) list(user_id = x, name = NA_character_,
+      character(), skip = 8, nmax = 10, quiet = TRUE)),
+    error = function(e) tibble::tibble(user_id = x, name = NA_character_,
       screen_name = NA_character_))
-  if (is.list(o)) {
+  if (is.data.frame(o)) {
     return(o)
   }
-  list(user_id = x,
-    name = paste(sub("<title>", "", o[1], fixed = TRUE), o[2]),
-    screen_name = sub(")", "", sub("(@", "", o[3], fixed = TRUE), fixed = TRUE))
+  o <- paste(o, collapse = " ")
+  tibble::tibble(user_id = x,
+    name = tfse::regmatches_first(o, "(?<=title>)[^(]+(?= \\()"),
+    screen_name = tfse::regmatches_first(o, "(?<=\\(@)\\S+(?=\\))")
+  )
 }
 
 #' Status ID to screen name
 #'
 #' Converts Twitter status ID to screen name
 #'
-#' @param twid Input status ID
+#' @param x Input status ID
 #' @return Screen name
 #' @export
-sid2sn <- function(twid) {
+sid2sn <- function(x) {
+  do.call("rbind", dapr::lap(x, sid2sn_))
+}
+
+sid2sn_ <- function(twid) {
   o <- tryCatch(
     httr::HEAD(paste0("https://twitter.com/NA/status/", twid))[["url"]],
-    error = function(e) list(screen_name = NA_character_))
-  if (is.list(o)) {
+    error = function(e) tibble::tibble(screen_name = NA_character_))
+  if (is.data.frame(o)) {
     return(o)
   }
   o <- sub("https://twitter.com/", "", o, fixed = TRUE)
-  sn2id(sub("/.*", "", o))
+  do.call("rbind", sn2id(sub("/.*", "", o)))
 }
